14f6699d7393ac98db207a065e132b1e1c4aede6
Lucio Camargo <luciomcamargo@gmail.com>
2018-11-24 19:58:05 +0100

    update

---
+++ dist/precache-manifest.afc143e964022bcda1e814f1be9e7882.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "3436bba2face8b45d131",
+    "url": "/static/js/main.3436bba2.chunk.js"
+  },
+  {
+    "revision": "9cb228f8513f7b47bdf1",
+    "url": "/static/js/1.9cb228f8.chunk.js"
+  },
+  {
+    "revision": "3436bba2face8b45d131",
+    "url": "/static/css/main.969b329b.chunk.css"
+  },
+  {
+    "revision": "cda0c06512bff491c5836bec8afa7297",
+    "url": "/index.html"
+  }
+];
